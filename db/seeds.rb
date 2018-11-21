f7a036451f044b161984cc6958445ff1ad25f096
suraj vingataredy <surajv4.0@gmail.com>
2018-11-21 17:20:40 +0100

    end of exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+5.times do
+  Restaurant.create!(
+    name: Faker::GameOfThrones.city,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+  )
+end
+puts 'Finished!'
